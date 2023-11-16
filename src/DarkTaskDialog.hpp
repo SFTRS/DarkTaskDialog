5b742c69229620589ff360760849f09c3a91da92
Oleg Korzhukov <msn@korzhukov.com>
2023-11-16 07:58:20 +0200

    Improved variable and struct naming for clarity

--- src/DarkTaskDialog.hpp
+++ src/DarkTaskDialog.hpp
@@ -51,7 +51,7 @@ namespace SFTRS {
 
             /* DATA STORAGE */
             static Theme theme;
-            struct originalCllbackData
+            struct TaskDalogCallbackWrap
             {
                 PFTASKDIALOGCALLBACK pfCallback;
                 LONG_PTR lpCallbackData;
@@ -80,11 +80,6 @@ namespace SFTRS {
             /* GRAPHICS HELPERS */
 
             const BLENDFUNCTION blend_f = { AC_SRC_OVER ,0, 255, AC_SRC_ALPHA };
-            /*
-        {
-            .SourceConstantAlpha = 255,
-            .AlphaFormat = AC_SRC_ALPHA
-        };*/
 
             struct MemoryBitmapAndDC
             {
@@ -119,23 +114,22 @@ namespace SFTRS {
 
             };
 
-
             Gdiplus::Bitmap* GdiBitmapFromHBITMAPwithAlpha(HBITMAP source, int width, int height)
             {
-                Gdiplus::Bitmap* retVal = new Gdiplus::Bitmap(width, height, PixelFormat32bppARGB);
+                Gdiplus::Bitmap* bitmap = new Gdiplus::Bitmap(width, height, PixelFormat32bppARGB);
 
                 Gdiplus::BitmapData target_info;
                 Gdiplus::Rect rect(0, 0, width, height);
 
-                Gdiplus::Status result = retVal->LockBits(&rect, Gdiplus::ImageLockModeWrite, PixelFormat32bppARGB, &target_info);
+                Gdiplus::Status result = bitmap->LockBits(&rect, Gdiplus::ImageLockModeWrite, PixelFormat32bppARGB, &target_info);
                 if (result != Gdiplus::Ok || target_info.Stride != width * 4)
                     return NULL;
 
                 GetBitmapBits(source, width * 4 * height, target_info.Scan0);
 
-                retVal->UnlockBits(&target_info);
+                bitmap->UnlockBits(&target_info);
 
-                return retVal;
+                return bitmap;
             }
 
             void AdjustBrightness(HDC hdc, LPCRECT rect, HBITMAP bitmap = NULL, float f = -0.78)
@@ -160,17 +154,16 @@ namespace SFTRS {
 
                 Gdiplus::ColorMatrix colorMatrix =
                 {
-                    1, 0, 0, 0, 0,  // R
-                    0, 1, 0, 0, 0,  // G
-                    0, 0, 1, 0, 0,  // B
-                    0, 0, 0, 1, 0,  // A
-                    f, f, f, 0, 1   // W
+                    1, 0, 0, 0, 0,
+                    0, 1, 0, 0, 0,
+                    0, 0, 1, 0, 0,
+                    0, 0, 0, 1, 0,
+                    f, f, f, 0, 1
                 };
                 Gdiplus::ImageAttributes imageAttr;
                 imageAttr.SetColorMatrix(&colorMatrix, Gdiplus::ColorMatrixFlagsDefault, Gdiplus::ColorAdjustTypeBitmap);
                 graphics.DrawImage(bmp, gdiRect, 0, 0, gdiRect.Width, gdiRect.Height, Gdiplus::UnitPixel, &imageAttr);
 
-                //cleanup
                 delete bmp;
             }
 
@@ -211,9 +204,7 @@ namespace SFTRS {
                         trueGetThemeColor(hTheme, iPartId, iStateId, TMT_TEXTCOLOR, &color);
                         color = increaseBrightness(color);
                     }
-
                     DTTOPTS options = { sizeof(DTTOPTS), DTT_TEXTCOLOR, color };
-      
                     retVal = trueDrawThemeTextEx(hTheme, hdc, iPartId, iStateId, pszText, cchText, dwTextFlags, (LPRECT)pRect, &options);
                 }
                 else
@@ -229,11 +220,8 @@ namespace SFTRS {
                 {
                     DTTOPTS options = pOptions ? *pOptions : DTTOPTS{ sizeof(DTTOPTS) };
                     options.dwFlags |= DTT_TEXTCOLOR;
-
-                    //second line of a Command Button
                     trueGetThemeColor(hTheme, iPartId, iStateId, TMT_TEXTCOLOR, &options.crText);
                     options.crText = increaseBrightness(options.crText);
-
                     return trueDrawThemeTextEx(hTheme, hdc, iPartId, iStateId, pszText, cchText, dwTextFlags, pRect, &options);
                 }
                 else
@@ -243,46 +231,47 @@ namespace SFTRS {
             HRESULT WINAPI myDrawThemeBackgroundEx(HTHEME hTheme, HDC hdc, int iPartId, int iStateId, LPCRECT pRect, const DTBGOPTS* pOptions)
             {
                 if (!painting)
-                    trueDrawThemeBackgroundEx(hTheme, hdc, iPartId, iStateId, pRect, pOptions);
+                    return trueDrawThemeBackgroundEx(hTheme, hdc, iPartId, iStateId, pRect, pOptions);
 
                 HRESULT retVal = S_OK;
 
                 switch (iPartId)
                 {
-                case TDLG_PRIMARYPANEL:
-                    FillRect(hdc, pRect, notSoDarkBrush);
-                    break;
-                case TDLG_FOOTNOTEPANE:
-                case TDLG_SECONDARYPANEL:
-                    FillRect(hdc, &pOptions->rcClip, darkBrush);
-                    break;
-                case TDLG_FOOTNOTESEPARATOR:
-                    retVal = trueDrawThemeBackgroundEx(hTheme, hdc, iPartId, iStateId, pRect, pOptions);
-                    AdjustBrightness(hdc, pRect);
-                    break;
-                case TDLG_EXPANDOBUTTON:
-                {
-                    // Windows 11 has button without background which becomes unrecognizable on dark background, so we invert button, cannot use the same technique on Windows 10, as button's border become chipped
-                    static enum { yes, no, unknown } mustInvertButton = unknown;
-                    if (mustInvertButton == unknown)
+                    case TDLG_PRIMARYPANEL:
+                        FillRect(hdc, pRect, notSoDarkBrush);
+                        break;
+                    case TDLG_FOOTNOTEPANE:
+                    case TDLG_SECONDARYPANEL:
+                        FillRect(hdc, &pOptions->rcClip, darkBrush);
+                        break;
+                    case TDLG_FOOTNOTESEPARATOR:
+                        retVal = trueDrawThemeBackgroundEx(hTheme, hdc, iPartId, iStateId, pRect, pOptions);
+                        AdjustBrightness(hdc, pRect);
+                        break;
+                    case TDLG_EXPANDOBUTTON:
                     {
-                        trueDrawThemeBackgroundEx(hTheme, hdc, iPartId, iStateId, pRect, pOptions);
-                        int buttonCenterX = pOptions->rcClip.left + (pOptions->rcClip.right - pOptions->rcClip.left) / 2;
-                        int buttonCenterY = pOptions->rcClip.top + (pOptions->rcClip.bottom - pOptions->rcClip.top) / 2;
-                        COLORREF centerPixel = GetPixel(hdc, buttonCenterX, buttonCenterY);
-                        mustInvertButton = centerPixel == white ? no : yes;
+                        // Windows 11 has button without background which becomes unrecognizable on dark background, so we invert button, cannot use the same technique on Windows 10, as button's border become chipped
+                        static enum { yes, no, unknown } mustInvertButton = unknown;
+                        if (mustInvertButton == unknown)
+                        {
+                            trueDrawThemeBackgroundEx(hTheme, hdc, iPartId, iStateId, pRect, pOptions);
+                            int buttonCenterX = pOptions->rcClip.left + (pOptions->rcClip.right - pOptions->rcClip.left) / 2;
+                            int buttonCenterY = pOptions->rcClip.top + (pOptions->rcClip.bottom - pOptions->rcClip.top) / 2;
+                            COLORREF centerPixel = GetPixel(hdc, buttonCenterX, buttonCenterY);
+                            mustInvertButton = centerPixel == white ? no : yes;
+                        }
+                        FillRect(hdc, pRect, darkBrush);
+                        if (mustInvertButton == yes) InvertRect(hdc, pRect);
+                        retVal = trueDrawThemeBackgroundEx(hTheme, hdc, iPartId, iStateId, pRect, pOptions);
+                        if (mustInvertButton == yes) InvertRect(hdc, pRect);
+                        break;
                     }
-                    FillRect(hdc, pRect, darkBrush);
-                    if (mustInvertButton == yes) InvertRect(hdc, pRect);
-                    retVal = trueDrawThemeBackgroundEx(hTheme, hdc, iPartId, iStateId, pRect, pOptions);
-                    if (mustInvertButton == yes) InvertRect(hdc, pRect);
-                    break;
-                }
-                case BP_CHECKBOX:
-                    if (HTHEME darkButtonTheme = OpenThemeData(NULL, L"DarkMode_Explorer::Button"))
+                    case BP_CHECKBOX:
                     {
-                        retVal = trueDrawThemeBackgroundEx(darkButtonTheme, hdc, iPartId, iStateId, pRect, pOptions);
-                        CloseThemeData(darkButtonTheme);
+                        HTHEME darkButtonTheme = OpenThemeData(NULL, L"DarkMode_Explorer::Button");
+                        retVal = trueDrawThemeBackgroundEx(darkButtonTheme ? darkButtonTheme : hTheme, hdc, iPartId, iStateId, pRect, pOptions);
+                        if (darkButtonTheme)
+                            CloseThemeData(darkButtonTheme);
                     }
                 }
                 return retVal;
@@ -293,11 +282,10 @@ namespace SFTRS {
                 if ((iPartId == BP_COMMANDLINKGLYPH || iPartId == BP_COMMANDLINK) && painting && iStateId != PBS_DISABLED)
                 {
                     MemoryBitmapAndDC canvas(pRect);
-                    HRESULT retVal = trueDrawThemeBackground(hTheme, canvas.DC, iPartId, iStateId, &canvas.rect, NULL);
+                    HRESULT result = trueDrawThemeBackground(hTheme, canvas.DC, iPartId, iStateId, &canvas.rect, NULL);
                     AdjustBrightness(canvas.DC, &canvas.rect, canvas.hBitmap, iPartId == BP_COMMANDLINK ? 1.0f : 0.35f);
                     AlphaBlend(hdc, pRect->left, pRect->top, canvas.width, canvas.height, canvas.DC, 0, 0, canvas.width, canvas.height, blend_f);
-
-                    return retVal;
+                    return result;
                 }
 
                 return trueDrawThemeBackground(hTheme, hdc, iPartId, iStateId, pRect, pClipRect);
@@ -307,17 +295,15 @@ namespace SFTRS {
 
             HWND WINAPI myCreateWindowEx(DWORD dwExStyle, LPCWSTR lpClassName, LPCWSTR lpWindowName, DWORD dwStyle, int X, int Y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
             {
-                HWND retVal = trueCreateWindowEx(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
-
+                HWND hwnd = trueCreateWindowEx(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
                 if (HIWORD(lpClassName) != 0 && std::wstring(lpClassName) == WC_LINK)
                 {
-                    // early subclassing is required to prevent blinking on page switch
-                    SetWindowSubclass(retVal, Subclassproc, (UINT_PTR)retVal, 0);
+                    SetWindowSubclass(hwnd, Subclassproc, (UINT_PTR)hwnd, 0);
                 }
-                return retVal;
+                return hwnd;
             }
 
-            /* ALL TD CHILDREN SUBCLASSINGS AND CALLBACKS */
+            /* ALL CHILDREN SUBCLASSINGS AND CALLBACKS */
 
             HRESULT CALLBACK taskdialogcallback(HWND, UINT, WPARAM, LPARAM, LONG_PTR);
 
@@ -326,7 +312,7 @@ namespace SFTRS {
                 if (uMsg == TDM_NAVIGATE_PAGE)
                 {
                     TASKDIALOGCONFIG* trueCongif = (TASKDIALOGCONFIG*)lParam;
-                    originalCllbackData* trueCallBackData = (originalCllbackData*)dwRefData;
+                    TaskDalogCallbackWrap* trueCallBackData = (TaskDalogCallbackWrap*)dwRefData;
                     trueCallBackData->pfCallback = trueCongif ? trueCongif->pfCallback : NULL;
                     trueCallBackData->lpCallbackData = trueCongif ? trueCongif->lpCallbackData : NULL;
 
@@ -371,7 +357,7 @@ namespace SFTRS {
 
                     if (wndClass(hWnd) == WC_LINK)
                     {
-                        return 1; // do not erase backround for links or they will blink white on extender and page switch
+                        return 1; // Do not erase backround for links or they will blink white on extender and page switch
                     }
 
                     break;
@@ -391,12 +377,11 @@ namespace SFTRS {
             BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM lParam)
             {
                 Reason reason = (Reason)lParam;
-
                 EnumChildWindows(hwnd, EnumChildProc, lParam);
 
-                auto className = wndClass(hwnd);
+                std::wstring className = wndClass(hwnd);
 
-                if (className == WC_LINK) // make links the same color as text
+                if (className == WC_LINK)
                 {
                     LITEM linkChanges = { LIF_ITEMINDEX | LIF_STATE, 0, theme==dark?LIS_DEFAULTCOLORS:0, LIS_DEFAULTCOLORS };
                     while (SendMessage(hwnd, LM_SETITEM, 0, (LPARAM)&linkChanges))
@@ -419,7 +404,6 @@ namespace SFTRS {
                 if (className == WC_BUTTON || className == WC_SCROLLBAR)
                 {
                     SetWindowTheme(hwnd, theme == dark ? L"DarkMode_Explorer" : NULL, NULL);
-                    return TRUE;
                 }
 
                 if (className == L"DirectUIHWND")
@@ -454,7 +438,6 @@ namespace SFTRS {
 
                 DetourTransactionBegin();
                 DetourUpdateThread(GetCurrentThread());
-
                 if (mustBeSet)
                 {
                     DetourAttach(&(PVOID&)trueGetThemeColor, myGetThemeColor);
@@ -489,10 +472,9 @@ namespace SFTRS {
                 {
                     update(hwnd, NewPage);
                 }
-                if (msg == TDN_CREATED) // called only once
+                if (msg == TDN_CREATED)
                 {
                     taskDialogs.insert(hwnd);
-
                     SetWindowSubclass(hwnd, Subclassproc, (UINT_PTR)hwnd, lpRefData);
                 }
                 if (msg == TDN_DESTROYED)
@@ -500,9 +482,8 @@ namespace SFTRS {
                     taskDialogs.erase(hwnd);
                 }
 
-                auto trueCallBackData = (originalCllbackData*)lpRefData;
-
-                if (trueCallBackData->pfCallback != NULL)
+                TaskDalogCallbackWrap* trueCallBackData = (TaskDalogCallbackWrap*)lpRefData;
+                if (trueCallBackData->pfCallback)
                     trueCallBackData->pfCallback(hwnd, msg, wParam, lParam, trueCallBackData->lpCallbackData);
 
                 return S_OK;
@@ -512,16 +493,13 @@ namespace SFTRS {
             HRESULT WINAPI myTaskDialogIndirect(const TASKDIALOGCONFIG* pTaskConfig, int* pnButton, int* pnRadioButton, BOOL* pfVerificationFlagChecked)
             {
                 ensureDetoursSet(true);
-
-                originalCllbackData callBackData = { pTaskConfig->pfCallback, pTaskConfig->lpCallbackData };
+                TaskDalogCallbackWrap callBackData = { pTaskConfig->pfCallback, pTaskConfig->lpCallbackData };
                 TASKDIALOGCONFIG myConfig = *pTaskConfig;
                 myConfig.lpCallbackData = (LONG_PTR)&callBackData;
                 myConfig.pfCallback = taskdialogcallback;
-                HRESULT retVal = trueTaskDialogIndirect(&myConfig, pnButton, pnRadioButton, pfVerificationFlagChecked);
-
+                HRESULT result = trueTaskDialogIndirect(&myConfig, pnButton, pnRadioButton, pfVerificationFlagChecked);
                 ensureDetoursSet();
-
-                return retVal;
+                return result;
             }
         }
 
@@ -547,7 +525,6 @@ namespace SFTRS {
             for (HWND hwnd : detail::taskDialogs)
             {
                 detail::update(hwnd, detail::ThemeSwith);
-                SendMessage(hwnd, WM_PAINT, 0, 0);
             }
         }
     }
